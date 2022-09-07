83e042fd6c46d03cf3c180fe3593423afd37c6fa
SN <sniescior160@gmail.com>
2022-09-07 14:41:09 +0100

    Passwords are now being hashed

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const { isEmail } = require('validator')
+const bcrypt = require('bcrypt')
 
 const userSchema = mongoose.Schema({
     "username": {
@@ -19,6 +20,21 @@ const userSchema = mongoose.Schema({
         required: [true, 'Please set your password'],
         minlength: [8, 'Password should have at least 8 characters']
     }
+})
+
+// mongoose hooks
+// fire a function after doc saved to db
+userSchema.post('save', function (doc, next) {
+    console.log('New user was created and saved', doc);
+    next()
+})
+
+// fire a function before user was saved to the db
+userSchema.pre('save', async function (next) {
+    const salt = await bcrypt.genSalt()
+    this.password = await bcrypt.hash(this.password, salt)
+    console.log('user about to be created and saved', this);
+    next()
 })
 
 const User = mongoose.model('user', userSchema)
