f46eb58ec57b5406aa5bbdc09c67c016eb825bb9
SN <sniescior160@gmail.com>
2022-09-12 00:31:25 +0100

    Basic setting page front-end

--- public/js/app.js
+++ public/js/app.js
@@ -6,3 +6,20 @@ function toggleButton(elementId) {
         element.classList.add('active')
     }
 }
+
+function toggleNestedMenu(elementId) {
+    const element = document.getElementById(elementId)
+    const appContainer = document.getElementById('appContainer')
+    const elementLinkId = elementId + '-link'
+    const elementLink = document.getElementById(elementLinkId)
+    console.log(elementLink);
+    if(element.classList.contains('hidden')) {
+        element.classList.remove('hidden')
+        appContainer.classList.add('hidden')
+        elementLink.classList.add('move-right')
+    } else {
+        element.classList.add('hidden')
+        appContainer.classList.remove('hidden')
+        elementLink.classList.remove('move-right')
+    }
+}
