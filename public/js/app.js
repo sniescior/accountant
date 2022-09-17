5b9e48112b66eb16667b9c9def014f2a821df0bb
SN <sniescior160@gmail.com>
2022-09-18 00:09:49 +0100

    New CSR implementation for settings page

--- public/js/app.js
+++ public/js/app.js
@@ -4,19 +4,6 @@ const appContainer = document.getElementById('appContainer')
 const overlay = document.getElementById('overlay')
 var url = window.location
 
-// gonna be different behaviour when page opened in Safari Browser
-var isSafari = window['safari']
-if(isSafari) {
-    appContainer.classList.add('safari')
-    Array.from(document.getElementsByClassName('nested-menu')).forEach((element) => {
-        element.classList.add('safari')
-    })
-
-    Array.from(document.getElementsByClassName('menu-link')).forEach((element) => {
-        element.classList.add('safari')
-    })
-}
-
 // Switch button function (for example dark-mode toggle button)
 function toggleButton(elementId) {
     const element = document.getElementById(elementId)
@@ -26,128 +13,6 @@ function toggleButton(elementId) {
         element.classList.add('active')
     }
 }
-
-// When user goes from one settings screen to another (for example #configuration -> #currency)
-function toggleNestedMenu(elementId, goToMainSettingsScreen = true, buttonUsed = true) {
-    const element = document.getElementById(elementId)
-    const elementLinkId = elementId + '-link'
-    const elementLink = document.getElementById(elementLinkId)
-
-    if(element.classList.contains('hidden')) {
-        element.classList.remove('hidden')
-        document.body.classList.add('hidden')
-        appContainer.classList.add('hidden')
-
-        if(elementLink) {
-            elementLink.classList.add('move-right')
-        }
-    } else {
-        if(goToMainSettingsScreen) {
-            document.body.classList.remove('hidden')
-            appContainer.classList.remove('hidden')
-        }
-        element.classList.add('hidden')
-        
-        if(elementLink) {
-            elementLink.classList.remove('move-right')
-        }
-    }
-}
-
-// Extend accordion menu
-function toggleDetailedMenu(elementId) {
-    const element = document.getElementById(elementId)
-    if(element.classList.contains('extended')) {
-        element.classList.remove('extended')
-    } else {
-        element.classList.add('extended')
-    }
-}
-
-window.onload = (e) => {
-    if(url.href.endsWith('/settings') || url.href.endsWith('/settings/')) {
-        document.location = location.protocol + '//' + location.hostname + ':3000/app/settings' + '#main'
-        return
-    }
-    if(url.href.endsWith('#main')) {
-        return
-    }
-    if(url.href.includes('#')) {
-        const nestedMenuId = url.href.split('#')[1]
-        toggleNestedMenu(nestedMenuId)
-    }
-}
-
-// open nestedmenu when certain url 
-function setNestedMenu(nestedMenuId, goToMainSettingsScreen = true, buttonUsed = true) {
-    const nestedMenu = document.getElementById(nestedMenuId)
-    if(nestedMenu.classList.contains('hidden')) {
-        toggleNestedMenu(nestedMenuId, goToMainSettingsScreen, buttonUsed)
-    }
-}
-
-// Listen to url changes (navigation observer)
-window.addEventListener('popstate', function (e) {
-
-    // user is at #main screen -> close all nested menus that might have been opened
-    // and bring the main screen to its initial appearance
-    if(url.href.endsWith('#main')) {
-        const nestedMenuList = document.getElementsByClassName('nested-menu')
-        const nestedMenuLinks = document.getElementsByClassName('move-right')
-        document.body.classList.remove('hidden')
-        appContainer.classList.remove('hidden')
-
-        // All nestedMenuLists back to the initial position (hidden)
-        Array.from(nestedMenuList).forEach((element) => {
-            if(!element.classList.contains('hidden')) {
-                element.classList.add('hidden')
-            }
-        })
-        
-        // All nestedMenuLinks back to the initial position
-        Array.from(nestedMenuLinks).forEach((element) => {
-            element.classList.remove('move-right')
-        })
-    }
-
-    // some nestedMenus fire up other nestedMenus
-    // for example: from configuration screen two nestedMenus could be opened (#repeat, #currency)
-    if(url.href.endsWith('#configuration')) {
-        const repeatNestedMenu = document.getElementById('repeat')
-        const currencyNestedMenu = document.getElementById('currency')
-        if(!repeatNestedMenu.classList.contains('hidden')) {
-            toggleNestedMenu(repeatNestedMenu.id, false, false)
-            return
-        }
-        
-        if(!currencyNestedMenu.classList.contains('hidden')) {
-            toggleNestedMenu(currencyNestedMenu.id, false, false)
-            return
-        }
-        
-        setNestedMenu('configuration', true, false)
-    }
-    
-    if(url.href.endsWith('#repeat')) {
-        setNestedMenu('repeat', false, false)
-    }
-    
-    if(url.href.endsWith('#currency')) {
-        setNestedMenu('currency', false, false)
-    }
-    
-    if(url.href.endsWith('#accounts')) {
-        setNestedMenu('accounts', true, false)
-    }
-    
-    if(url.href.endsWith('#plan')) {
-        setNestedMenu('plan', true, false)
-    }
-    
-    if(url.href.endsWith('#security')) {
-        setNestedMenu('security', true, false)
-    }
-});
 
 // MARK: Pop-Ups handling
 function openPopUp(elementId) {
@@ -187,4 +52,10 @@ overlay.addEventListener('click', (e) => {
 function expandOptions(elementID) {
     const element = document.getElementById(elementID)
     element.classList.toggle('focus')
+}
+
+const toggleDetailedMenu = (elementID) => {
+    console.log(elementID);
+    const element = document.getElementById(elementID)
+    element.classList.toggle('extended')
 }
---
+++ public/js/settings.js
@@ -0,0 +1,96 @@
+
+// gonna be different behaviour when page opened in Safari Browser
+var isSafari = window['safari']
+if(isSafari) {
+    appContainer.classList.add('safari')
+    Array.from(document.getElementsByClassName('nested-menu')).forEach((element) => {
+        element.classList.add('safari')
+    })
+
+    Array.from(document.getElementsByClassName('menu-link')).forEach((element) => {
+        element.classList.add('safari')
+    })
+}
+
+const nestedMenus = document.getElementsByClassName('nested-menu')
+
+const closeNestedMenus = () => {
+    Array.from(nestedMenus).forEach(nestedMenu => {
+        if(!nestedMenu.classList.contains('hidden')) {
+            nestedMenu.classList.add('hidden')
+        }
+    })
+}
+
+const route = (siteID, parentID) => {
+    console.log('Routing', siteID);
+    const appContainer = document.getElementById('appContainer')
+    const nestedMenu = document.getElementById(siteID)
+    if(siteID == 'main') {
+        closeNestedMenus()
+        document.body.classList.remove('hidden')
+        appContainer.classList.remove('hidden')
+        return
+    }
+
+    if(parentID) {
+        document.body.classList.add('hidden')
+        appContainer.classList.add('hidden')
+
+        nestedMenu.classList.remove('hidden')
+        return
+    }
+
+    closeNestedMenus()
+    document.body.classList.add('hidden')
+    appContainer.classList.add('hidden')
+    nestedMenu.classList.remove('hidden')
+}
+
+const navigateTo = url => {
+    console.log('Navigating to ', url);
+    history.pushState(null, null, url)
+    settingsClientRouter()
+}
+
+const settingsClientRoutes = [
+    { path: '/app/settings', view: 'main' },
+    { path: '/app/settings/configuration', view: 'configuration' },
+    { path: '/app/settings/configuration/repeat', view: 'repeat' },
+    { path: '/app/settings/configuration/currency', view: 'currency' },
+    { path: '/app/settings/accounts', view: 'accounts' },
+    { path: '/app/settings/security', view: 'security' },
+    { path: '/app/settings/plan', view: 'plan' },
+    { path: '/app/settings/language', view: 'language' }
+]
+
+const settingsClientRouter = async () => {
+    const potentialMatches = settingsClientRoutes.map(route => {
+        return {
+            route: route,
+            isMatch: location.pathname === route.path
+        }
+    })
+
+    let match = potentialMatches.find(potentialMatch => potentialMatch.isMatch)
+    console.log(match);
+    route(match.route.view)
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('DOM Loaded');
+    settingsClientRouter()
+    document.addEventListener('click', (e) => {
+        if(e.target.matches('[data-link]')) {
+            e.preventDefault()
+            navigateTo(e.target.href)
+        }
+    })
+
+    window.addEventListener('popstate', () => {
+        console.log('Popstate detected')
+        console.log(location.pathname);
+
+        settingsClientRouter()
+    })
+})
