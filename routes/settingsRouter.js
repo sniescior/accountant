e7c4e334b83dd46592cb8bc4876a8c50fc2d9db1
Szymon N <szymon@MacBook-Pro-Szymon.local>
2022-10-11 13:48:42 +0200

    Income / Expense categories list on settings page

--- routes/settingsRouter.js
+++ routes/settingsRouter.js
@@ -1,8 +1,28 @@
 const express = require('express')
 const router = express.Router()
+const incomeCategory = require('../models/incomeCategory')
+const expenseCategory = require('../models/expenseCategory')
+const { default: mongoose } = require('mongoose')
 
-router.get('/', (req, res) => {
-    res.render('app/settings', { user: req.user })
+router.get('/', async (req, res) => {
+    
+    const userID = mongoose.Types.ObjectId(req.user.id)
+    const incomeCategories = await incomeCategory.find({ userID: userID })
+    const expenseCategories = await expenseCategory.find({ userID: userID })
+
+    const context = {
+        incomeCategories: incomeCategories,
+        expenseCategories: expenseCategories
+    }
+
+    // const newIncomeCategory = new incomeCategory({
+    //     userID: req.user.id,
+    //     name: 'New Income Category'
+    // })
+
+    // await newIncomeCategory.save()
+
+    res.render('app/settings', { user: req.user, context: context })
 })
 
 module.exports = router
